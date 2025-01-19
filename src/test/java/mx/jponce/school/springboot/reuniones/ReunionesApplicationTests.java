5eabed0e34287a4fec578c96cf46041de775e64d
PacoChuy <ponce.jesusf@gmail.com>
2025-01-18 22:56:20 -0600

    first commit

---
+++ src/test/java/mx/jponce/school/springboot/reuniones/ReunionesApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.jponce.school.springboot.reuniones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReunionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
