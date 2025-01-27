38bcc5fe95f194852e03811840e82f090ca5db66
11112487 <jesus.f.ponce@autozone.com>
2025-01-27 10:47:05 -0600

    SBcourse - AuteticationCotroller added

--- src/main/java/mx/jponce/school/springboot/reuniones/config/SecurityConfig.java
+++ src/main/java/mx/jponce/school/springboot/reuniones/config/SecurityConfig.java
@@ -4,6 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -26,6 +32,23 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails usuario1 = User.builder()
+                .username("jponce")
+                .password(passwordEncoder().encode("ssshhh"))
+                .roles("USER","API_USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(usuario1);
+    }
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
